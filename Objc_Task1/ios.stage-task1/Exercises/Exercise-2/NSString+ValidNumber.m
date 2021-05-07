56ec860d89b0e780d91f6333a904c8fccde47d62
kryshe4ka <kryshe4ka@gmail.com>
2021-05-07 17:06:22 +0300

    feat: solve ValidNumber task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,21 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    if (self == nil || [self  isEqual: @""]) {
+        return false;
+    }
+    
+    NSCharacterSet *decimalDigitCharacterSet = NSCharacterSet.decimalDigitCharacterSet;
+    unichar symbol;
+    
+    for (int i = 0; i < self.length; i++ ) {
+        symbol = [self characterAtIndex:i];
+        if (![decimalDigitCharacterSet characterIsMember:symbol]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 @end
