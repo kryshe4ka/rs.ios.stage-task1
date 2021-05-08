69d42d0a09bd94939deb6e90adff8db732d1c914
kryshe4ka <kryshe4ka@gmail.com>
2021-05-08 11:52:36 +0300

    feat: solve MinRotated task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,48 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self == nil || self.count == 0) {
+        return nil;
+    }
+    
+    // если в массиве всего один объект -> возвращаем его
+    if (self.count == 1) {
+        return [self objectAtIndex:0];
+    }
+   
+    // Простой поиск минимального числа в массиве. Сложность O(n)
+//    NSNumber *min = self.firstObject;
+//    for (NSNumber *number in self) {
+//        if (number.intValue < min.intValue) {
+//            min = number;
+//        }
+//    }
+//    return min;
+    
+    // Модификация алгоритма бинарного поиска для циклично сдвинутого массива. Сложность O(logN)
+    
+    // Пускай массив будет отсортирован. Делим массив на две половины, отбрасываем не нужную, оставшуюся опять делим на две части и так пока не найдем нужное значение.
+    
+    NSUInteger low = 0;
+    NSUInteger high = self.count - 1;
+    NSInteger middle;
+
+    while ([self[low] integerValue] > [self[high] integerValue]) {
+        // Оператор двоичного правого сдвига >>. Значение левого операнда перемещается вправо на количество битов, указанное правым операндом. Например, при сдвиге на 2 из 0011 1100 поучим 0000 1111.
+        // обычный способ mid = (left + right) / 2 вызовет переполнение на больших массивах
+        // и поэтому будем использовать более правильный метод: mid = left + (right - left) / 2
+        // >> 1 same as "(low + high) / 2", but avoid negative overflow and should be faster than "low + (high - low)/2"
+        middle = (low + high) >> 1;
+        
+        // Теперь, осталось определить, в какой части массива нужно искать искомый минимальный элемент
+        if ([self[middle] integerValue] > [self[high] integerValue]) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    }
+    return [self objectAtIndex:low];
+    
 }
 
 @end
