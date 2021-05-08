9a5358f89ea39949ab9c6faf1baa1393c5453538
kryshe4ka <kryshe4ka@gmail.com>
2021-05-08 12:41:50 +0300

    feat: solve numberConverter task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,27 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    
+    NSMutableArray *characterArray = [[NSMutableArray alloc] init];
+    
+    if (number == nil) {
+        return characterArray;
+    }
+    
+    // если число отрицательное, то делаем его положительным. С модулем сделать не вышло, какая-то ерунда на выходе была.
+    if (number.intValue < 0 ) {
+        number = @(-number.intValue);
+    }
+    // записываем последнюю цифру в массив и делим число на 10
+    while (number.intValue > 0) {
+        NSInteger digit = number.intValue % 10;
+        NSString *symbol = [NSString stringWithFormat:@"%lx", digit];
+        // конвертировать NSInteger в NSString
+        [characterArray addObject:symbol];
+        number = @(number.intValue / 10);
+    }
+    
+    return characterArray;
 }
 
 @end
